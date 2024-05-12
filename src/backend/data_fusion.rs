3d115bdd1e424a47dd6a8871171cca533cdc53c3
Tyr Chen <tyr.chen@gmail.com>
2024-05-12 16:46:18 -0700

    feature: rewrite describe function

--- src/backend/data_fusion.rs
+++ src/backend/data_fusion.rs
@@ -1,10 +1,19 @@
-use arrow::util::pretty::pretty_format_batches;
-use datafusion::execution::{
-    config::SessionConfig,
-    context::SessionContext,
-    options::{CsvReadOptions, NdJsonReadOptions},
+use arrow::{
+    array::{ArrayRef, RecordBatch, StringArray},
+    compute::{cast, concat},
+    datatypes::{DataType, Field, Schema, SchemaRef},
+    util::pretty::pretty_format_batches,
 };
-use std::ops::Deref;
+use datafusion::{
+    dataframe::DataFrame,
+    execution::{
+        config::SessionConfig,
+        context::SessionContext,
+        options::{CsvReadOptions, NdJsonReadOptions},
+    },
+    logical_expr::{avg, case, col, count, is_null, lit, max, median, min, stddev, sum},
+};
+use std::{ops::Deref, sync::Arc};
 
 use crate::{
     cli::{ConnectOpts, DatasetConn},
@@ -23,7 +32,6 @@ impl DataFusionBackend {
 }
 
 impl Backend for DataFusionBackend {
-    type DataFrame = datafusion::dataframe::DataFrame;
     async fn connect(&mut self, opts: &ConnectOpts) -> anyhow::Result<()> {
         match &opts.conn {
             DatasetConn::Postgres(_conn_str) => {
@@ -56,24 +64,27 @@ impl Backend for DataFusionBackend {
         Ok(())
     }
 
-    async fn list(&self) -> anyhow::Result<Self::DataFrame> {
+    async fn list(&self) -> anyhow::Result<impl ReplDisplay> {
         let sql = "select table_name, table_type from information_schema.tables where table_schema = 'public'";
         let df = self.0.sql(sql).await?;
         Ok(df)
     }
 
-    async fn schema(&self, name: &str) -> anyhow::Result<Self::DataFrame> {
+    async fn schema(&self, name: &str) -> anyhow::Result<impl ReplDisplay> {
         let df = self.0.sql(&format!("DESCRIBE {}", name)).await?;
         Ok(df)
     }
 
-    async fn describe(&self, name: &str) -> anyhow::Result<Self::DataFrame> {
+    async fn describe(&self, name: &str) -> anyhow::Result<impl ReplDisplay> {
         let df = self.0.sql(&format!("select * from {}", name)).await?;
-        let df = df.describe().await?;
-        Ok(df)
+        // let df = df.describe().await?;
+        let ddf = DescribeDataFrame::new(df);
+        let record_batch = ddf.to_record_batch().await?;
+        println!("{:?}", record_batch.num_columns());
+        Ok(record_batch)
     }
 
-    async fn head(&self, name: &str, size: usize) -> anyhow::Result<Self::DataFrame> {
+    async fn head(&self, name: &str, size: usize) -> anyhow::Result<impl ReplDisplay> {
         let df = self
             .0
             .sql(&format!("SELECT * FROM {} LIMIT {}", name, size))
@@ -81,7 +92,7 @@ impl Backend for DataFusionBackend {
         Ok(df)
     }
 
-    async fn sql(&self, sql: &str) -> anyhow::Result<Self::DataFrame> {
+    async fn sql(&self, sql: &str) -> anyhow::Result<impl ReplDisplay> {
         let df = self.0.sql(sql).await?;
         Ok(df)
     }
@@ -101,10 +112,212 @@ impl Deref for DataFusionBackend {
     }
 }
 
-impl ReplDisplay for datafusion::dataframe::DataFrame {
+impl ReplDisplay for DataFrame {
     async fn display(self) -> anyhow::Result<String> {
         let batches = self.collect().await?;
         let data = pretty_format_batches(&batches)?;
         Ok(data.to_string())
+    }
+}
+
+impl ReplDisplay for RecordBatch {
+    async fn display(self) -> anyhow::Result<String> {
+        let data = pretty_format_batches(&[self])?;
+        Ok(data.to_string())
+    }
+}
+
+struct DescribeDataFrame {
+    df: DataFrame,
+    functions: &'static [&'static str],
+    schema: SchemaRef,
+}
+
+impl DescribeDataFrame {
+    fn new(df: DataFrame) -> Self {
+        let functions = &["count", "null_count", "mean", "std", "min", "max", "median"];
+
+        let original_schema_fields = df.schema().fields().iter();
+
+        //define describe column
+        let mut describe_schemas = vec![Field::new("describe", DataType::Utf8, false)];
+        describe_schemas.extend(original_schema_fields.clone().map(|field| {
+            if field.data_type().is_numeric() {
+                Field::new(field.name(), DataType::Float64, true)
+            } else {
+                Field::new(field.name(), DataType::Utf8, true)
+            }
+        }));
+        Self {
+            df,
+            functions,
+            schema: Arc::new(Schema::new(describe_schemas)),
+        }
+    }
+
+    fn count(&self) -> anyhow::Result<DataFrame> {
+        let original_schema_fields = self.df.schema().fields().iter();
+        let ret = self.df.clone().aggregate(
+            vec![],
+            original_schema_fields
+                .clone()
+                .map(|f| count(col(f.name())).alias(f.name()))
+                .collect::<Vec<_>>(),
+        )?;
+        Ok(ret)
+    }
+
+    fn null_count(&self) -> anyhow::Result<DataFrame> {
+        let original_schema_fields = self.df.schema().fields().iter();
+        let ret = self.df.clone().aggregate(
+            vec![],
+            original_schema_fields
+                .clone()
+                .map(|f| {
+                    sum(case(is_null(col(f.name())))
+                        .when(lit(true), lit(1))
+                        .otherwise(lit(0))
+                        .unwrap())
+                    .alias(f.name())
+                })
+                .collect::<Vec<_>>(),
+        )?;
+        Ok(ret)
+    }
+
+    fn mean(&self) -> anyhow::Result<DataFrame> {
+        let original_schema_fields = self.df.schema().fields().iter();
+        let ret = self.df.clone().aggregate(
+            vec![],
+            original_schema_fields
+                .clone()
+                .filter(|f| f.data_type().is_numeric())
+                .map(|f| avg(col(f.name())).alias(f.name()))
+                .collect::<Vec<_>>(),
+        )?;
+        Ok(ret)
+    }
+
+    fn stddev(&self) -> anyhow::Result<DataFrame> {
+        let original_schema_fields = self.df.schema().fields().iter();
+        let ret = self.df.clone().aggregate(
+            vec![],
+            original_schema_fields
+                .clone()
+                .filter(|f| f.data_type().is_numeric())
+                .map(|f| stddev(col(f.name())).alias(f.name()))
+                .collect::<Vec<_>>(),
+        )?;
+        Ok(ret)
+    }
+
+    fn min(&self) -> anyhow::Result<DataFrame> {
+        let original_schema_fields = self.df.schema().fields().iter();
+        let ret = self.df.clone().aggregate(
+            vec![],
+            original_schema_fields
+                .clone()
+                .filter(|f| !matches!(f.data_type(), DataType::Binary | DataType::Boolean))
+                .map(|f| min(col(f.name())).alias(f.name()))
+                .collect::<Vec<_>>(),
+        )?;
+        Ok(ret)
+    }
+
+    fn max(&self) -> anyhow::Result<DataFrame> {
+        let original_schema_fields = self.df.schema().fields().iter();
+        let ret = self.df.clone().aggregate(
+            vec![],
+            original_schema_fields
+                .clone()
+                .filter(|f| !matches!(f.data_type(), DataType::Binary | DataType::Boolean))
+                .map(|f| max(col(f.name())).alias(f.name()))
+                .collect::<Vec<_>>(),
+        )?;
+        Ok(ret)
+    }
+
+    fn medium(&self) -> anyhow::Result<DataFrame> {
+        let original_schema_fields = self.df.schema().fields().iter();
+        let ret = self.df.clone().aggregate(
+            vec![],
+            original_schema_fields
+                .clone()
+                .filter(|f| f.data_type().is_numeric())
+                .map(|f| median(col(f.name())).alias(f.name()))
+                .collect::<Vec<_>>(),
+        )?;
+        Ok(ret)
+    }
+
+    async fn to_record_batch(&self) -> anyhow::Result<RecordBatch> {
+        let original_schema_fields = self.df.schema().fields().iter();
+
+        let batches = vec![
+            self.count(),
+            self.null_count(),
+            self.mean(),
+            self.stddev(),
+            self.min(),
+            self.max(),
+            self.medium(),
+        ];
+
+        // first column with function names
+        let mut describe_col_vec: Vec<ArrayRef> = vec![Arc::new(StringArray::from(
+            self.functions
+                .iter()
+                .map(|v| v.to_string())
+                .collect::<Vec<_>>(),
+        ))];
+        for field in original_schema_fields {
+            let mut array_data = vec![];
+            for result in batches.iter() {
+                let array_ref = match result {
+                    Ok(df) => {
+                        let batchs = df.clone().collect().await;
+                        match batchs {
+                            Ok(batchs)
+                                if batchs.len() == 1
+                                    && batchs[0].column_by_name(field.name()).is_some() =>
+                            {
+                                let column = batchs[0].column_by_name(field.name()).unwrap();
+                                if field.data_type().is_numeric() {
+                                    cast(column, &DataType::Float64)?
+                                } else {
+                                    cast(column, &DataType::Utf8)?
+                                }
+                            }
+                            _ => Arc::new(StringArray::from(vec!["null"])),
+                        }
+                    }
+                    //Handling error when only boolean/binary column, and in other cases
+                    Err(err)
+                        if err.to_string().contains(
+                            "Error during planning: \
+                                            Aggregate requires at least one grouping \
+                                            or aggregate expression",
+                        ) =>
+                    {
+                        Arc::new(StringArray::from(vec!["null"]))
+                    }
+                    Err(other_err) => {
+                        panic!("{other_err}")
+                    }
+                };
+                array_data.push(array_ref);
+            }
+            describe_col_vec.push(concat(
+                array_data
+                    .iter()
+                    .map(|af| af.as_ref())
+                    .collect::<Vec<_>>()
+                    .as_slice(),
+            )?);
+        }
+
+        let batch = RecordBatch::try_new(self.schema.clone(), describe_col_vec)?;
+
+        Ok(batch)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -17,13 +17,12 @@ trait CmdExector {
 }
 
 trait Backend {
-    type DataFrame: ReplDisplay;
     async fn connect(&mut self, opts: &ConnectOpts) -> anyhow::Result<()>;
-    async fn list(&self) -> anyhow::Result<Self::DataFrame>;
-    async fn schema(&self, name: &str) -> anyhow::Result<Self::DataFrame>;
-    async fn describe(&self, name: &str) -> anyhow::Result<Self::DataFrame>;
-    async fn head(&self, name: &str, size: usize) -> anyhow::Result<Self::DataFrame>;
-    async fn sql(&self, sql: &str) -> anyhow::Result<Self::DataFrame>;
+    async fn list(&self) -> anyhow::Result<impl ReplDisplay>;
+    async fn schema(&self, name: &str) -> anyhow::Result<impl ReplDisplay>;
+    async fn describe(&self, name: &str) -> anyhow::Result<impl ReplDisplay>;
+    async fn head(&self, name: &str, size: usize) -> anyhow::Result<impl ReplDisplay>;
+    async fn sql(&self, sql: &str) -> anyhow::Result<impl ReplDisplay>;
 }
 
 trait ReplDisplay {
